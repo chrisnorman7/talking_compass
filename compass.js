285d00f5961ccc423ee8c8fe9aaf0cf20c47f303
Chris Norman <chris.norman2@googlemail.com>
2020-02-27 16:34:01 +0000

    Added some real features.

--- compass.js
+++ compass.js
@@ -1,6 +1,7 @@
 const long = 200
 const short = 50
 const between = 100
+const startCompassText = "Start Compass"
 
 const directions = {
     0: ["North", [long]],
@@ -14,46 +15,142 @@ const directions = {
 }
 
 const startstop = document.querySelector("#startstop")
+const save = document.querySelector("#save")
+const savedCoordinates = document.querySelector("#savedCoordinates")
+const distanceDirections = document.querySelector("#distanceDirections")
+
 const direction = document.querySelector("#direction")
+const position = document.querySelector("#position")
+
+let latitude = null
+let savedLatitude = null
+let longitude = null
+let savedLongitude = null
+
+const gpsNames = ["latitude", "longitude", "altitude", "heading", "speed", "accuracy"]
+
+let watchId = null
+
 let lastDirectionTime = 0
 let lastVibrationTime = 0
 const directionInterval = 2000
 const vibrationInterval = 2000
 
+window.onload = () => {
+    position.hidden = true
+    startstop.value = startCompassText
+}
+
 startstop.onclick = () => {
-    if (window.DeviceOrientationEvent !== undefined) {
-        window.addEventListener('deviceorientation', event => {
-            let compassDir = null
-            if(event.alpha === undefined) {
-                // Stupid Apple.
-                compassDir = event.webkitCompassHeading;  
-            } else {
-                compassDir = event.alpha
-            }
-            if (compassDir === null) {
-                direction.innerText = "Unknown direction."
-            } else {
-                compassDir = Math.floor(compassDir)
-                let cardinal = directions[compassDir]
-                let now = new Date().getTime()
-                if (cardinal === undefined) {
-                    if ((now - lastDirectionTime) > directionInterval) {
-                        lastDirectionTime = now
-                        direction.innerText = compassDir
-                    }
-                } else {
-                    let [name, pattern] = cardinal
-                    direction.innerText = name
-                    if ((now - lastVibrationTime) > vibrationInterval) {
-                        lastVibrationTime = now
-                        navigator.vibrate(pattern)
-                    }
-                }
-            }
-        })
+    if (watchId) {
+        navigator.geolocation.clearWatch(watchId)
+        watchId = null
+        startstop.value = startCompassText
     } else {
-        direction.innerText = "Compass data is not supported by your browser."
+        if ("geolocation" in navigator) {
+            position.hidden = false
+            watchId = navigator.geolocation.watchPosition(
+                obj => {
+                    latitude = obj.coords.latitude
+                    longitude = obj.coords.longitude
+                    for (let name of gpsNames) {
+                        let text = obj.coords[name]
+                        if (text === null) {
+                            text = "Unknown"
+                        }
+                        document.querySelector(`#${name}`).innerText = text
+                    }
+                    let compassDir = obj.coords.heading
+                    if (compassDir === null) {
+                        direction.innerText = "Unknown direction."
+                    } else {
+                        compassDir = Math.floor(compassDir)
+                        let cardinal = directions[compassDir]
+                        let now = new Date().getTime()
+                        if (cardinal === undefined) {
+                            if ((now - lastDirectionTime) > directionInterval) {
+                                lastDirectionTime = now
+                                direction.innerText = compassDir
+                            }
+                        } else {
+                            let [name, pattern] = cardinal
+                            direction.innerText = name
+                            if ((now - lastVibrationTime) > vibrationInterval) {
+                                lastVibrationTime = now
+                                navigator.vibrate(pattern)
+                            }
+                        }
+                    }
+                    if (savedLatitude && savedLongitude) {
+                        updateDistanceDirections()
+                    }
+                },
+                error => {
+                    direction.innerText = `Error: ${error.message}`
+                    if (error.code == 1) {
+                        startstop.value = startCompassText
+                        watchId = null
+                    }
+                },
+                {enableHighAccuracy: true}
+            )
+            navigator.vibrate([1000])
+            startstop.value = "Compass started"
+        } else {
+            direction.innerText = "Location information is not supported by your browser."
+        }
     }
-    navigator.vibrate([1000])
-    startstop.value = "Compass started"
+}
+
+save.onclick = () => {
+    if (latitude === null && longitude === null) {
+        alert("You must first start the compass.")
+    } else {
+        savedLatitude = latitude
+        savedLongitude = longitude
+        savedCoordinates.innerText = `${savedLatitude}° latitude, ${savedLongitude}° longitude.`
+        updateDistanceDirections()
+    }
+}
+
+function degreesToRadians(degrees) {
+    return degrees * (Math.PI / 180)
+}
+
+function bearing(lat1, lon1, lat2, lon2){
+    const y = Math.sin(lon2 - lon1) * Math.cos(lat2)
+    const x = (
+        Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) *
+        Math.cos(lon2 - lon1)
+    )
+    let brng = Math.atan2(y, x)
+    brng = radiansToDegrees(brng)
+    return (brng + 360) % 360
+}
+
+function radiansToDegrees(radians) {
+    return radians * 180 / Math.PI;
+}
+
+function distanceBetween(lat1, lon1, lat2, lon2) {
+    const R = 6371e3; // metres
+    const fi1 = degreesToRadians(lat1)
+    const fi2 = degreesToRadians(lat2)
+    const deltaLambda = degreesToRadians(lon2 - lon1)
+    return Math.acos(
+        Math.sin(fi1) * Math.sin(fi2) + Math.cos(fi1) * Math.cos(fi2) *
+        Math.cos(deltaLambda)
+    ) * R
+}
+
+function updateDistanceDirections() {
+    let distance = distanceBetween(
+        latitude, longitude,
+        savedLatitude, savedLongitude
+    )
+    let degrees = bearing(
+        latitude, longitude,
+        savedLatitude, savedLongitude
+    )
+    distanceDirections.innerText = `${distance} meters at ${degrees}°.`
 }
